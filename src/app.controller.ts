8a26c8fd6cdabac6fb77313984d5159d5e386182
anurag khandelwal <anurag_khandelwal@outlook.com>
2023-05-26 01:10:32 +0530

    Configure user routes

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,41 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
-
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { UserService } from './services/user.service';
+import { User, Prisma } from '@prisma/client';
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly userService: UserService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('users')
+  async getAllUsers(
+    @Param('pagination')
+    pagination: {
+      skip: number;
+      take: number;
+      orderBy: Prisma.UserOrderByWithRelationInput;
+    },
+  ): Promise<User[]> {
+    return this.userService.getAllUsers(pagination);
   }
+
+  @Post('create-user')
+  async addNewUser(@Body() userData: User): Promise<User> {
+    return this.userService.createUser(userData);
+  }
+
+  @Put('user/:id')
+  async updateUser(
+    @Param('id') id: string,
+    @Body() userData: User,
+  ): Promise<User> {
+    return this.userService.updateUser({
+      where: { id: Number(id) },
+      data: userData,
+    });
+  }
+
+  @Delete('user/:id')
+  async deleteUser(@Param('id') id:string): Promise<User>{
+    return this.userService.deleteUser({id: parseInt(id)});
+  }
+
 }
